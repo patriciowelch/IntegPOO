c54aea14364e762a8c18aae604b768430f0d7dce
Patricio Welch <patriciowelch@gmail.com>
2024-10-27 13:08:32 -0300

    inicios de robot

---
+++ servidor/src/robot.py
@@ -0,0 +1,9 @@
+class Robot():
+    def __init__():
+        pass
+    def conectar(self):
+        pass
+    def desconectar(self):
+        pass
+    def enviar_comando(self, comando):
+        pass
