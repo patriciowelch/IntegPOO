441e76aa06aa589140f767c39043494e8539ba2d
Patricio Welch <patriciowelch@gmail.com>
2024-11-04 16:46:20 -0300

    soniditos

--- servidor/src/robot.py
+++ servidor/src/robot.py
@@ -3,7 +3,7 @@ from log import Log
 import json
 import sounddevice as sd
 import numpy as np
-import time
+import threading
 
 class Robot():
     def __init__(self, timeout=1,velMax=100):
@@ -16,6 +16,7 @@ class Robot():
         self._velMax = velMax
         self.serial = None
         self.motor = False
+        self.sound_thread = None
         self.log = Log("Log_Robot")
         pass
 
@@ -43,6 +44,7 @@ class Robot():
             if self.serial is None:
                 self.addToLog(f"Conectando al puerto {self._puerto}...")
                 self.serial = serial.Serial(self._puerto, self._baudrate, timeout=self._timeout)
+                self.sonido("conectar")
                 self.serial.readline().decode().strip()
                 mensaje = ""
                 while True:
@@ -77,6 +79,7 @@ class Robot():
             try:
                 self.serial.close()
                 self.serial = None
+                self.sonido("desconectar")
                 self.addToLog(f"Desconectado del puerto {self._puerto}.")
                 return "Desconectado"
             except serial.SerialException as e:
@@ -93,25 +96,42 @@ class Robot():
             return (self.enviar_comando('M5'))
         elif arg == 'cerrar':
             return (self.enviar_comando('M3'))
+        
+    def sonido_no_bloqueante(self, sound):
+        waves=[]
+        for i in range(sound[3]):
+            t= np.linspace(0,sound[0],int(sound[2]*sound[0]))
+            wave = sound[0]*np.sin(2*np.pi*sound[1]*t)
+            waves.append(wave)
+            pause = np.zeros(int(sound[2] * sound[4]))
+            waves.append(pause)
+        full_wave = np.concatenate(waves)
+        sd.play(full_wave,samplerate=sound[2])
+        sd.wait()
             
     def sonido(self, arg):
         if arg == 'conectar':
-            sound = [0.5,550,44100,1,0.2]
-        for i in range(sound[3]):
-            t= np.linspace(0,sound[0],int(sound[2]*sound[0]),endpoint=False)
-            wave = sound[0]*np.sin(2*np.pi*sound[1]*t)
-            sd.play(wave,samplerate=sound[2])
-            sd.wait()
-            time.sleep(sound[4])
+            sound = [0.2,1318.51,44100,2,0.05]
+        elif arg == 'desconectar':
+            sound = [0.40,1046.5,44100,1,0]
+        elif arg == 'Error':
+            sound = [0.8,1975.53,44100,1,0.07]
+        elif arg == 'comando':
+            sound = [0.4,587.33,44100,3,0.07]
+        self.sound_thread = threading.Thread(target=self.sonido_no_bloqueante, args=(sound,))
+        self.sound_thread.start()
+        
         
     def enviar_comando(self, comando):
         if self.serial is None:
+            self.sonido("Error")
             raise Exception("Error: El puerto serie no esta conectado")
         else:
             self.addToLog(f"Enviando comando: {comando}")
             try:
                 comando += '\r'
                 self.serial.write(comando.encode())
+                self.sonido("comando")
                 self.addToLog(f"Comando enviado: {comando}")
             
             except serial.SerialException as e:
@@ -130,6 +150,8 @@ class Robot():
                     if info != "":
                         self.addToLog(info)
                         mensaje += info+'\n'
+                        if "error" in info.lower():
+                            self.sonido("Error")
                     else :
                         break
                 return mensaje
@@ -172,6 +194,7 @@ class Robot():
 
     def desactivar_motor(self):
         if self.serial is None:
+            self.sonido("Error")
             return "No hay conexión serial abierta"
         else:
             self.motor = False
@@ -179,6 +202,7 @@ class Robot():
 
     def activar_motor(self):
         if self.serial is None:
+            self.sonido("Error")
             return "No hay conexión serial abierta"
         else:
             self.motor = True
